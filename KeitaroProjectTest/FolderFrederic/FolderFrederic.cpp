2b6bdd64ca9832722049456cc24963d2030c0a8f
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-02-25 15:37:04 +0000

    haw

---
+++ KeitaroProjectTest/FolderFrederic/FolderFrederic.cpp
@@ -0,0 +1,9 @@
+//
+//  FolderFrederic.cpp
+//  KeitaroProjectTest
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//  Copyright © 2019 Keitaro Takahashi. All rights reserved.
+//
+
+#include "FolderFrederic.hpp"
---
+++ KeitaroProjectTest/FolderFrederic/FolderFrederic.hpp
@@ -0,0 +1,14 @@
+//
+//  FolderFrederic.hpp
+//  KeitaroProjectTest
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//  Copyright © 2019 Keitaro Takahashi. All rights reserved.
+//
+
+#ifndef FolderFrederic_hpp
+#define FolderFrederic_hpp
+
+#include <stdio.h>
+
+#endif /* FolderFrederic_hpp */
