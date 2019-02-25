8ecb0716f98f6f1f99733fe086df612939ea7e46
Keitaro Takahashi <neoterize@me.com>
2019-02-25 15:33:09 +0000

    Initial Commit

---
+++ KeitaroProjectTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KeitaroProjectTest
+//
+//  Created by Keitaro on 25/02/2019.
+//  Copyright © 2019 Keitaro Takahashi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
