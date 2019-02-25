e256f98e7055e87056b8bc96c44003fe66629e34
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-02-25 16:17:35 +0000

    FredericClass added

---
+++ KeitaroProjectTest/FredericClass/FredericClass.cpp
@@ -0,0 +1,9 @@
+//
+//  FredericClass.cpp
+//  KeitaroProjectTest
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//  Copyright © 2019 Keitaro Takahashi. All rights reserved.
+//
+
+#include "FredericClass.hpp"
---
+++ KeitaroProjectTest/FredericClass/FredericClass.hpp
@@ -0,0 +1,14 @@
+//
+//  FredericClass.hpp
+//  KeitaroProjectTest
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//  Copyright © 2019 Keitaro Takahashi. All rights reserved.
+//
+
+#ifndef FredericClass_hpp
+#define FredericClass_hpp
+
+#include <stdio.h>
+
+#endif /* FredericClass_hpp */
